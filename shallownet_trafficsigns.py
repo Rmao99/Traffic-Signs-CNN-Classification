626e6d0f564b5e19f074e3a0d3fd0f6766e8af84
Richard Mao <richard.mao.123@gmail.com>
2018-01-28 15:35:38 -0500

    Added plotting function at end to keep track of training loss/accuracy

--- shallownet_trafficsigns.py
+++ shallownet_trafficsigns.py
@@ -129,3 +129,16 @@ print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=32)
 print(classification_report(testY.argmax(axis=1),
 	predictions.argmax(axis=1), target_names=labelNames))
+
+#plot training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 40), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 40), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 40), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, 40), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.show()
